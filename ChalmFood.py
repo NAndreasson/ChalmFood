64c0e0a29f5055bd8f065372758809c813042b8c
Niklas Andréasson <eaglus_@hotmail.com>
2013-01-14 21:16:10 +0100

    Got rid of ascii encoding error, instead I'm encountering another

--- ChalmFood.py
+++ ChalmFood.py
@@ -1,7 +1,28 @@
 import sublime, sublime_plugin
 import threading
-import urllib
-from HTMLParser import HTMLParser
+import urllib2
+from HTMLParser import HTMLParser as parser
+
+
+class ChalmFoodHTMLParser(parser):
+    def __init__(self):
+        parser.__init__(self)
+        self.recording = 0
+        self.data = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "ul":
+            for name, value in attrs:
+                if name == "id" and value == "K":
+                    self.recording = 1
+
+    def handle_endtag(self, tag):
+        if tag == "ul":
+            self.recording -= 1
+
+    def handle_data(self, data):
+        if self.recording:
+            self.data.append(data)
 
 
 class ChalmFoodCommand(sublime_plugin.TextCommand):
@@ -10,6 +31,7 @@ class ChalmFoodCommand(sublime_plugin.TextCommand):
         thread = DataGetter(5)
         thread.start()
 
+
 class DataGetter(threading.Thread):
     def __init__(self, timeout):
         self.timeout = timeout
@@ -17,30 +39,12 @@ class DataGetter(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-        parser = ChalmFoodHTMLParser()
-        siteReq = urllib.urlopen("http://chalmerskonferens.se/dagens-menyer/johanneberg/")
+        pt = ChalmFoodHTMLParser()
+        siteReq = urllib2.urlopen("http://chalmerskonferens.se/dagens-menyer/johanneberg/")
         self.result = siteReq.read()
-        parser.feed(self.result)
-        print parser.data
-        parser.close()
+        self.result = self.result.decode("utf-8")
+        print self.result
+        pt.feed(self.result)
+        print pt.data
+        pt.close()
         siteReq.close()
-
-class ChalmFoodHTMLParser(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.recording = 0
-        self.data = []
-
-    def handle_starttag(self, tag, attrs):
-        if tag == 'ul':
-            for name, value in attrs:
-                if name == 'id' and value == 'K':
-                    self.recording = 1
-
-    def handle_endtag(self, tag):
-        if tag == 'ul':
-            self.recording -= 1
-
-    def handle_data(self, data):
-        if self.recording:
-            self.data.append(data)
