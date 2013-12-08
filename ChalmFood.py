9a56bccb48850e8b6322bf6e40336f3fa520f9c4
Niklas Andreasson <eaglus_@hotmail.com>
2013-12-08 21:48:14 +0100

    Plugin seems to run in sublime text 3 now

--- ChalmFood.py
+++ ChalmFood.py
@@ -1,30 +1,30 @@
 import sublime, sublime_plugin
 import threading
-import urllib2
+import urllib.request
+import urllib.error
 import json
 
 class ChalmFoodCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        # self.view.insert(edit, 0, "Hello, World!")
-        thread = DataGetter(5)
-        thread.start()
-        self.handle_threads(edit, thread)
+        thread = DataGetter(10)
 
-    def handle_threads(self, edit, threads):
-        if threads.is_alive():
-            sublime.set_timeout(lambda: self.handle_threads(edit, threads), 100)
-            return
+        thread.start()
+        thread.join()
+
+        self.handle_result(edit, thread.result)
+
+    def handle_result(self, edit, result):
         sublime.status_message('Chalm food successfully ran the request')
-        print threads.result
-        result = threads.result
+
         for restaurant in result:
-            title = result[restaurant]["title"]
+            title = restaurant["title"]
             self.view.insert(edit, 0, "= " +  title + " =\n")
-            
-            dishes = result[restaurant]["dishes"]
+
+            dishes = restaurant["dishes"]
             for dish in dishes:
                 self.view.insert(edit, self.view.size(), "\t" + dish["title"] + "\n")
                 self.view.insert(edit, self.view.size(), "\t\t" + dish["desc"] + "\n")
+            # self.view.insert(edit, self.view.size(), "\n\n")
 
 
 class DataGetter(threading.Thread):
@@ -34,14 +34,14 @@ class DataGetter(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-        try: 
-            siteReq = urllib2.urlopen("http://localhost:5000/johanneberg", timeout=self.timeout)
-            self.result = json.load(siteReq)
+        try:
+            siteReq = urllib.request.urlopen("http://vps.nandreasson.se:5000/johanneberg", timeout=self.timeout)
+            self.result = json.loads( siteReq.readall().decode("utf-8") )
             siteReq.close()
             return
-        except (urllib2.HTTPError) as (e):  
-            err = '%s: HTTP error %s contacting API' % (__name__, str(e.code))  
-        except (urllib2.URLError) as (e):  
-            err = '%s: URL error %s contacting API' % (__name__, str(e.reason))  
-        sublime.error_message(err)  
-        self.result = False  
+        except urllib.error.HTTPError as e:
+            err = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
+        except urllib.error.URLError as e:
+            err = '%s: URL error %s contacting API' % (__name__, str(e.reason))
+        sublime.error_message(err)
+        self.result = False
